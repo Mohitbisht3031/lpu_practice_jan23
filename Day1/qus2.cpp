7ca2bddfd4dfe4d8d972741313daafc11dc8ba7f
unknown <imohiitsingh@gmail.com>
2023-01-10 21:22:10 +0530

    day1 practice

---
+++ Day1/qus2.cpp
@@ -0,0 +1,18 @@
+/*Check if a number is even or odd.*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    // if(num%2 == 0){
+    //     cout<<num<<" it is a even number"<<endl;
+    // }else cout<<num<<" it is a odd number"<<endl;
+
+    // a ? b : c
+    (num%2 == 0) ? cout<<num<<" it is a even num"<<endl : cout<<num<<" it is a odd num"<<endl;
+    return 0;
+}
