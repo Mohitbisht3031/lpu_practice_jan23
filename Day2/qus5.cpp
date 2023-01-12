b0787a4883c42f91e5c8665f9e06a8afe315953d
unknown <imohiitsingh@gmail.com>
2023-01-12 21:08:21 +0530

    day3

--- Day2/qus5.cpp
+++ Day2/qus5.cpp
@@ -3,14 +3,29 @@
 
 using namespace std;
 
-int skipX(int*arr,int x){
-    int size = sizeof(arr)/sizeof(arr[0]);
+// int skipX(int*arr,int x){
+//     int size = sizeof(arr)/sizeof(int);
+//     cout<<size<<endl;
+//     int sum = 0;
+//     for(int i = 0;i<size;i++){
+//         cout<<i<<" ";
+//         if(arr[i] == x){
+//             continue;
+//         }else sum+=arr[i];
+//         // cout<<"printing from for"<<endl;
+//     }
+//     return sum;
+// }
+
+int skipX(vector<int>arr,int x){
+    // int size = sizeof(arr)/sizeof(arr[0]);
     int sum = 0;
-    for(int i = 0;i<size;i++){
+    for(int i = 0;i<arr.size();i++){
+        // cout<<i<<" ";
         if(arr[i] == x){
             continue;
         }else sum+=arr[i];
-        cout<<"printing from for"<<endl;
+        // cout<<"printing from for"<<endl;
     }
     return sum;
 }
@@ -18,7 +33,8 @@ int skipX(int*arr,int x){
 int main(){
     int n ;
     cin>>n;
-    int arr[] = {1,2,3,5,4,7};
+    // int arr[] = {1,2,3,5,4,7};
+    vector<int>arr = {1,2,3,5,4,7};
 
     int ans = skipX(arr,n);
 
