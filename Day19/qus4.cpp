128fadfaa4d08a95a1edc6ca17117ea2152b0b3e
unknown <imohiitsingh@gmail.com>
2023-02-01 21:07:21 +0530

    day20

--- Day19/qus4.cpp
+++ Day19/qus4.cpp
@@ -5,7 +5,27 @@ using namespace std;
 
 
 int minFlip(string str){
+    stack<char>stk;
+    for(int i = 0;i<str.size();i++){
+        if(str[i] == '{')stk.push(str[i]);
+        else{
+            if(!stk.empty() && stk.top() == '{')stk.pop();
+            else stk.push(str[i]);
+        }
+    }
 
+    int c = 0;
+    while(!stk.empty()){
+        char ch1 = stk.top();
+        stk.pop();
+        char ch2 = stk.top();
+        stk.pop();
+        if(ch1 == ch2){
+            c++;
+        }else c+=2;
+    }
+
+    return c;
 }
 
 int main(){
