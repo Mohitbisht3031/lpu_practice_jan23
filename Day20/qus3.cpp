d42b78ee0462742a4a22e7ffc75302b51cc12486
unknown <imohiitsingh@gmail.com>
2023-02-03 21:08:14 +0530

    day21

--- Day20/qus3.cpp
+++ Day20/qus3.cpp
@@ -1,13 +1,1 @@
-/*print allsubset of arr who's sum is equal to K*/
-
-#include<bits/stdc++.h>
-using namespace std;
-
-vector<vector<int>> printAllSubset(vector<int>&arr,int k){
-
-}
-
-int main(){
-
-    return 0;
-}
+https://leetcode.com/problems/combination-sum/
---
+++ Day21/qus3.cpp
@@ -0,0 +1,25 @@
+/*https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Tnode{
+    public:
+    int val;
+    Tnode*left;
+    Tnode*right;
+    Tnode(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+    Tnode* lowestCommonAncestor(Tnode* root, Tnode* p, Tnode* q) {
+        
+    }
+
+int main(){
+
+    return 0;
+}
